dfab57a39224122d8c8b08f9670b6e497b4de31b
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-03 15:29:07 +0700

    Initial commit: Set up spring boot project; set up application.properties and create database named sof3061_01 for connecting project - DB

---
+++ src/main/java/com/example/sof3061_01/Sof306101Application.java
@@ -0,0 +1,14 @@
+package com.example.sof3061_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof306101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof306101Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/com/example/sof3061_01/Sof306101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sof3061_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof306101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
