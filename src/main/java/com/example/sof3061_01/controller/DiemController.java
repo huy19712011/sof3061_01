af6288e177666080cafd55c1508358fff0415c62
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-03 17:29:03 +0700

    Rest API: PUT for updating by {id}

--- src/main/java/com/example/sof3061_01/controller/DiemController.java
+++ src/main/java/com/example/sof3061_01/controller/DiemController.java
@@ -7,10 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,4 +34,13 @@ public class DiemController {
 
         return ResponseEntity.ok(diem);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Diem> updateDiem(@RequestBody Diem diem, @PathVariable("id") long id) {
+
+        Diem updatedDiem = diemService.updateDiem(diem, id);
+
+        return ResponseEntity.ok(updatedDiem);
+    }
+
 }
